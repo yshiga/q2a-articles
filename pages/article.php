48e84f320d07a57cb168a332554dfe61528d25a5
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-18 16:37:17 +0900

    yaml読み込みとページの存在チェック

--- pages/article.php
+++ pages/article.php
@@ -4,12 +4,36 @@
         header( 'Location: ../' );
         exit;
     }
+    require_once(ARTICLES_DIR .'/vendor/spyc/Spyc.php');
 
     $qa_content = qa_content_prepare(true);
     $page = qa_request_part(1);
-    $html = "";
-    $html .= "<p>".$page."</p>";
-    $qa_content['title'] = $page."のまとめページ";
-    $qa_content['custom'] = $html;
+
+    $ret = exists_article_page($page);
+    if (!empty($ret)) {
+        $qa_content['title'] = @$ret['title'];
+        $html = '<p>'.$page.'のまとめページ</p>';
+        $qa_content['custom'] = $html;
+    } else {
+        $qa_content = include QA_INCLUDE_DIR.'qa-page-not-found.php';
+    }
 
     return $qa_content;
+
+
+function articles_get_html_path($page)
+{
+    return ARTICLES_DIR . '/html/' . $page . '.html';
+}
+
+function exists_article_page($page)
+{
+    $articles = Spyc::YAMLLoad(ARTICLES_DIR . '/articles.yml');
+    $ret = array();
+    foreach ($articles as $article) {
+        if($article['path'] === $page) {
+            $ret = $article;
+        }
+    }
+    return $ret;
+}
