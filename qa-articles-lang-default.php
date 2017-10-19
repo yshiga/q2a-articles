4f188b752328e8bb3757c75b7c50a580c4938188
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-19 13:19:49 +0900

    翻訳修正

--- qa-articles-lang-default.php
+++ qa-articles-lang-default.php
@@ -2,7 +2,7 @@
 
 // default(Japanese)
 return array(
-    'articles_page_title' => 'まとめページ一覧',
+    'articles_page_title' => 'まとめ記事',
     'file_not_found' => 'ファイルが見つかりませんでした。',
-    'articles_not_found' => 'まとめページがありません。',
+    'articles_not_found' => 'まとめ記事がありません。',
 );
