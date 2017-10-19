d95a31b52e80881d7cd3ec8ec721e3e9700fc193
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-19 11:25:07 +0900

    sitemap 出力

--- pages/xml-sitemap.php
+++ pages/xml-sitemap.php
@@ -6,16 +6,23 @@
     }
 
     $siteurl = qa_opt( 'site_url' );
+    $ad = new ArticlesData(ARTICLES_YML);
+    $articles = $ad->get_articles();
 
     header( 'Content-type: text/xml; charset=utf-8' );
 
     echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
     echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
-    sitemap_output('article/hogehoge', 0.1);
+    // まとめページ
+    foreach ($articles as $article) {
+        sitemap_output('article/'.$article['path'], $siteurl, 0.1);
+    }
+    // 一覧ページ
+    sitemap_output('artcles', $siturl, 0.5);
     echo "</urlset>\n";
     return null;
 
-function sitemap_output( $request, $priority )
+function sitemap_output( $request, $siteurl, $priority )
 {
     echo "\t<url>\n" .
         "\t\t<loc>" . qa_xml( qa_path( $request, null, qa_opt( 'site_url' ) ) ) . "</loc>\n" .
