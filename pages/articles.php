12cc40f964e661609bfff043a9ebc6687492d393
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-11-21 14:47:03 +0900

    コラム一覧ページ

--- pages/articles.php
+++ pages/articles.php
@@ -5,19 +5,18 @@
         exit;
     }
 
+    require_once ARTICLES_DIR.'/articles-util.php';
+
     $qa_content = qa_content_prepare(true);
     $qa_content['title'] = qa_lang_html('articles_lang/articles_page_title'); 
 
     $ad = new ArticlesData(ARTICLES_YML);
     $articles = $ad->get_articles();
-    $html = "";
     if (!empty($articles)) {
-        $html = "<ul>";
-        foreach ($articles as $article) {
-            $url = qa_path('article/'.$article['path'], null, qa_opt('site_url'));
-            $html .= '<li><a href="'.$url.'">'.$article['title'].'</a></li>';
+        $html = '';
+        foreach($ad->get_articles() as $article) {
+            $html .= articles_util::get_list_item($article);
         }
-        $html .= "</ul>";
         $qa_content['custom'] = $html;
     } else {
         $qa_content['custom'] = qa_lang('articles_lang/articles_not_found');
