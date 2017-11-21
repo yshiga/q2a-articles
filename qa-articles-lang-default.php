66f9440aa5e5382c17dbaaf705d84e10bcfa1e97
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-11-21 14:37:40 +0900

    タイトル変更

--- qa-articles-lang-default.php
+++ qa-articles-lang-default.php
@@ -2,9 +2,9 @@
 
 // default(Japanese)
 return array(
-    'articles_page_title' => 'まとめ記事',
+    'articles_page_title' => 'コラム',
     'file_not_found' => 'ファイルが見つかりませんでした。',
-    'articles_not_found' => 'まとめ記事がありません。',
+    'articles_not_found' => 'コラムがありません。',
     'list_title' => '編集部コラム一覧',
     'updated_format' => '最終更新日： Y年m月d日',
 );
