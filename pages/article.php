068b55556c429e269eb3a89d1d9f609ba2ed0d59
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-11-21 11:13:45 +0900

    他のまとめ記事一覧

--- pages/article.php
+++ pages/article.php
@@ -18,6 +18,7 @@
         } else {
             $qa_content['custom'] = qa_lang('articles_lang/file_not_found');
         }
+        $qa_content['article_list'] = articles_get_list_html($ad, $page);
     } else {
         $qa_content = include QA_INCLUDE_DIR.'qa-page-not-found.php';
     }
@@ -30,4 +31,56 @@
 function articles_get_html_path($page)
 {
     return ARTICLES_DIR . '/html/' . $page . '.html';
+}
+
+function articles_get_list_html($ad, $page)
+{
+    $title = qa_lang('articles_lang/list_title');
+    $html = '<h3 class="mdl-typography--headline">'.$title.'</h3>';
+    foreach($ad->get_articles() as $article) {
+        if ($page !== $article['path']) {
+            $html .= articles_get_list_item($article);
+        }
+    }
+
+    return $html;
+}
+
+function articles_get_list_item($article)
+{
+    $template_path = articles_get_html_path('article_list_template');
+    $template = file_get_contents($template_path);
+    $url = qa_path('article/'.$article['path'], null, qa_opt('site_url'));
+    $file = articles_get_html_path($article['path']);
+    if (file_exists($file)) {
+        $html = file_get_contents($file);
+        if ($image) {
+            $image_style = 'background-image:url('.$image.');min-height:150px;';
+        } else {
+            $image_style = '';
+        }
+        $content = articles_get_item_content($html);
+        $updated = date ("更新日： Y年m月d日", filemtime($file));
+    } else {
+        return '';
+    }
+    $params = array(
+        '^url' => $url,
+        '^image_style' => $image_style,
+        '^title' => $article['title'],
+        '^content' => $content,
+        '^updated' => $updated,
+    );
+    return strtr($template, $params);
+}
+
+function articles_get_item_content($html)
+{
+    $regex = '/<p class=\"mdl-typography--subhead\">(.+)<\/p>/i';
+    if (preg_match($regex, $html, $matches)) {
+        $ret = $matches[1];
+    } else {
+        $ret = '';
+    }
+    return $ret;
 }
--- qa-articles-lang-default.php
+++ qa-articles-lang-default.php
@@ -5,4 +5,5 @@ return array(
     'articles_page_title' => 'まとめ記事',
     'file_not_found' => 'ファイルが見つかりませんでした。',
     'articles_not_found' => 'まとめ記事がありません。',
+    'list_title' => '編集部コラム一覧',
 );
